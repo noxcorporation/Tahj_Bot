b6155980c61637ad33a307c166534477a2dfa4d4
nox282 <nox.tux@gmail.com>
2017-06-29 23:53:15 -0400

    adding procfile

--- server.js
+++ server.js
@@ -6,4 +6,5 @@ var voiceChannelName = "Global";
 var aliasesFile = "alias.txt";
 var botToken = "MzMwMTgwNTYwNjQxMDY0OTcx.DDdQJw.UuZ6j-jIqQkWZ8fBAYEqWbVeeKo";
 
+bot.setYoutubeKey("AIzaSyC3k9Qc4_XsYJdadZq0a3jq68UYmzr3FZ4");
 bot.run(serverName, textChannelName, voiceChannelName, aliasesFile, botToken);
