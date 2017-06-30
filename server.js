a2ea9fc6fb676b3e9c98cc8577ee38ee25327ac0
nox282 <nox.tux@gmail.com>
2017-06-29 23:57:10 -0400

    editing bot config

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var bot = require("discord-music-bot");
 
 var serverName = "Les Lyth";
 var textChannelName = "robots";
-var voiceChannelName = "Global";
+var voiceChannelName = "Music";
 var aliasesFile = "alias.txt";
 var botToken = "MzMwMTgwNTYwNjQxMDY0OTcx.DDdQJw.UuZ6j-jIqQkWZ8fBAYEqWbVeeKo";
 
