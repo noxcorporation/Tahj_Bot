03c216970fd3a386446a69217c7b1be85fae1a31
nox282 <nox.tux@gmail.com>
2017-06-29 23:13:09 -0500

    adding initial files

---
+++ server.js
@@ -0,0 +1,9 @@
+var bot = require("discord-music-bot");
+
+var serverName = "Les Lyth";
+var textChannelName = "robots";
+var voiceChannelName = "Global";
+var aliasesFile = "alias.txt";
+var botToken = "MzMwMTgwNTYwNjQxMDY0OTcx.DDdQJw.UuZ6j-jIqQkWZ8fBAYEqWbVeeKo";
+
+bot.run(serverName, textChannelName, voiceChannelName, aliasesFile, botToken);
